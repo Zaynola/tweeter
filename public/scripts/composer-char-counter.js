3f1d61583b2e46765121548c3520dc2905111bb3
Zaynola <olaoluodushola@gmail.com>
2023-10-29 20:52:14 -0400

    implement the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    // Register an event handler for the input element inside the form in .new-tweet
+    $('.new-tweet form input').on('keyup', function () {
+        var inputValue = $(this).val();
+        var inputValueLength = inputValue.length;
+        console.log("Input value: " + inputValue);
+        console.log("Input value length: " + inputValueLength);
+
+        var charCount = 140 - inputValue.length;
+        $('.counter').text(charCount);
+
+        if (charCount < 0) {
+            $('.counter').addClass('invalid');
+        } else {
+            $('.counter').removeClass('invalid');
+        }
+    });
+});
